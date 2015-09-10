f7b00807ff6337b1470ab57b2888036384e485fb
asalas1093 <asalas@e112-m-05.mchenry.edu>
2015-09-10 17:00:02 -0500

    AlejandroS_CriticalThinking

---
+++ Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var itWorks = " This is my Robot!!"
+let longLine = "_____________________________________"
+let littleLine = "|         _________________         |"
+let sideLine = "|                                   |"
+let eyes = "|      ( o )               ( o )    |"
+let nose = "|                (  )               |"
+let longLineB = "|___________________________________|"
+
+
+println(longLine)
+println(sideLine)
+println(sideLine)
+println(eyes)
+println(sideLine)
+println(sideLine)
+println(sideLine)
+println(sideLine)
+println(nose)
+println(sideLine)
+println(sideLine)
+println(sideLine)
+println(littleLine)
+println(sideLine)
+println(sideLine)
+println(sideLine)
+println(longLineB)
+
+println(itWorks)
